fdcaf3fe4004389986b4c3f957f2c099f956e2b1
prem <premkattamanchi@gmail.com>
2023-04-27 00:09:26 +0530

    modified

--- src/main/java/com/driver/controllers/UserController.java
+++ src/main/java/com/driver/controllers/UserController.java
@@ -12,15 +12,15 @@ public class UserController {
     @Autowired
     UserServiceImpl userService;
     @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@RequestParam String username, @RequestParam String password, @RequestParam String countryName)throws Exception{
+    public ResponseEntity<Void> registerUser(@RequestParam String username, @RequestParam String password, @RequestParam String countryName)throws Exception{
         //create a user of given country. The originalIp of the user should be "countryCode.userId" and return the user. Note that right now user is not connected and thus connected would be false and maskedIp would be null
         //Note that the userId is created automatically by the repository layer
           User  user = userService.register(username, password, countryName);
-        return new ResponseEntity<>(user,HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping("/subscribe")
-    public void subscribe(@RequestParam Integer userId, @RequestParam Integer serviceProviderId)throws Exception{
+    public void subscribe(@RequestParam Integer userId, @RequestParam Integer serviceProviderId){
         //subscribe to the serviceProvider by adding it to the list of providers and return updated User
             User user = userService.subscribe(userId, serviceProviderId);
     }
